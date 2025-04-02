cce8c2880c2d2f51c3fa9fc9903a20abaad16fd0
unknown <USER@COSME.COSME>
2025-04-02 16:56:34 +0200

    exercice encour

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div> 
+    <a href="">Accueil</a>
+    <a href="">Profil</a>
+    <a href="">form</a>
+</div>
+  )
+}
+
+export default SideBar
+
