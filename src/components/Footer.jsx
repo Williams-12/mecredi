63bdc51cee0e8a4692dad7cbd87920f6b60ba418
Williams-12 <williamstchegoun30@gmail.com>
2025-04-02 15:53:19 +0100

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,13 @@ function Footer() {
   return (
     // footer
     <footer>
+        
         <div>
-        <a href="">Accueil</a>
+        <a href="">Accueil</a><br />
         <a href="">Profil</a>
     </div>
     <form action="">
+        <h4>Contactez-nous !</h4>
         <label>Nom: <input type="text" placeholder='votre nom' /></label><br />
         <label>Email: <input type="text" placeholder='Email' /></label><br />
         <button>Envoyez</button>
