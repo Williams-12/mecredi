f8ca41895a91ba66910998c8316edc4881c604c6
unknown <USER@COSME.COSME>
2025-04-03 16:22:29 +0200

    exercice encour

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-function SideBar() {
+const SideBar = () => {
   return (
-    <div> 
-    <a href="">Accueil</a>
-    <a href="">Profil</a>
-    <a href="">form</a>
-</div>
-  )
-}
+    <div className="d-flex flex-column bg-dark text-light p-3 vh-100" style={{ width: '250px' }}>
+      <h4 className="text-center mb-4">Menu</h4>
+      <nav className="nav flex-column">
+        <a href="/" className="nav-link text-light">Accueil</a>
+        <a href="/profil" className="nav-link text-light">Profil</a>
+        <a href="/form" className="nav-link text-light">Form</a>
+        <a href="/settings" className="nav-link text-light">Paramètres</a>
+      </nav>
+    </div>
+  );
+};
 
-export default SideBar
-
+export default SideBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import Footer from './components/partials/Footer.jsx'
+import SideBar from './components/SideBar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SideBar />
+
+
   </StrictMode>,
 )
